23714bd4c78e3f19cf23c2a49868a3a3f29020f8
manyvarsath <avatar@LAPTOP-ACGOV0AD.localdomain>
2025-07-12 21:26:37 +0100

    garbo

--- 10-factorial.js
+++ 10-factorial.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 
 function factorial(n) {
 	if (n < 0) return "NaN";
-	if (n === 0 || n === 1) return 1;
+	if (n === 0 || n === 1 || isNaN(n)) return 1;
 	return n * factorial(n - 1);
 }
 
