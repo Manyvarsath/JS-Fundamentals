21f61b3029d62c99184769f41899aff90d6f7c55
manyvarsath <avatar@LAPTOP-ACGOV0AD.localdomain>
2025-07-12 21:25:12 +0100

    garbo

--- 8-square.js
+++ 8-square.js
@@ -4,6 +4,6 @@ if (args.length === 0 || isNaN(args[0])) {
 	console.log("Missing size");
 } else {
 	for (let i = 0; i < parseInt(args[0]); i++) {
-		console.log("x".repeat(parseInt(args[0])));
+		console.log("X".repeat(parseInt(args[0])));
 	}
 }
